03da38edbc230aef083a7bc0df0804d931fa2044
2hoseong <leehoseong611@naver.com>
2025-09-15 14:50:04 +0900

    2025-09-15 수업시간

---
+++ Assets/Lecture03/Script/SpikeActtion.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SpikeAction : MonoBehaviour
+{
+    void Start()
+    {
+     
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - 0.05f, transform.position.y, transform.position.z);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("¹Ú¾Ò¾î!!!");
+
+        if (collision.gameObject.CompareTag("SpikeDestroyer"))
+        {
+            Destroy(gameObject);
+        }   
+    }
+}
